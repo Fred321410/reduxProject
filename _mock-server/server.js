0a3272418ec493d6bcb525593dffec215f039772
Fred321410 <matthieu.florentin@gmail.com>
2018-07-22 20:38:27 +0200

    Creation basic mock server

---
+++ _mock-server/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  http = require('http'),
+  path = require('path'),
+  bills = require('./routes/bills'),
+  cors = require('cors');
+
+var app = express();
+
+app.set('port', process.env.PORT || 9000);
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT, DELETE");
+  next();
+});
+
+// JSON API
+
+app.use('/api/bills', bills);
+
+
+app.listen(app.get('port'), function () {
+  console.log('Server listening on http://localhost:%d/', app.get('port'));
+});
