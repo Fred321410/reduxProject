5ead0c3d7a2ba80156e172d067fd4c882c41ba4e
Matthieu FLORENTIN <matthieu.florentin@netapsys.fr>
2018-07-27 17:56:42 +0200

    Debut tableau de bills

--- _mock-server/routes/bills.js
+++ _mock-server/routes/bills.js
@@ -8,7 +8,7 @@ router.use(function timeLog(req, res, next) {
 });
 // define the home page route
 router.get('/', function(req, res) {
-  res.status(200).json([{id: 1, description: 'Facture EDF'}, {id: 2, description: 'CB Carrefour'}]);
+  res.status(200).json([{id: 1, description: 'Facture EDF', amount: 155, date: '2018-07-13'}, {id: 2, description: 'CB Carrefour', amount: -25, date: '2018-07-14'}]);
 });
 
 module.exports = router;
--- src/app/bills/components/bill-preview-list.ts
+++ src/app/bills/components/bill-preview-list.ts
@@ -1,21 +1,49 @@
 import { Component, Input } from '@angular/core';
 import {Bill} from '../models/bill';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'rp-bill-preview-list',
-  template: `
-    <rp-bill-preview *ngFor="let bill of bills" [bill]="bill"></rp-bill-preview>
-  `,
-  styles: [
-    `
-    :host {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: center;
-    }
-  `,
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
   ],
+  template: `
+    <table mat-table [dataSource]='bills'  multiTemplateDataRows class='mat-elevation-z8'>
+      <ng-container matColumnDef="{{column}}" *ngFor="let column of columnsToDisplay">
+        <th mat-header-cell *matHeaderCellDef> {{column}} </th>
+        <td mat-cell *matCellDef="let element"> {{element[column]}} </td>
+      </ng-container>
+
+      <ng-container matColumnDef="expandedDetail">
+        <td mat-cell *matCellDef="let element" [attr.colspan]="columnsToDisplay.length">
+          <div class="example-element-detail"
+               [@detailExpand]="element == expandedElement ? 'expanded' : 'collapsed'">
+            <div class="example-element-description">
+              {{element.description}}
+            </div>
+          </div>
+        </td>
+      </ng-container>
+
+      <tr mat-header-row *matHeaderRowDef="columnsToDisplay"></tr>
+      <tr mat-row *matRowDef="let element; columns: columnsToDisplay;"
+          class="example-element-row"
+          [class.example-expanded-row]="expandedElement === element"
+          (click)="expandedElement = element">
+      </tr>
+      <tr mat-row *matRowDef="let row; columns: ['expandedDetail']" class="example-detail-row"></tr>
+    </table>
+<!--    <rp-bill-preview *ngFor="let bill of bills" [bill]="bill"></rp-bill-preview>-->
+  `,
+  styleUrls: ['./bill-preview-list.scss']
 })
 export class BillPreviewListComponent {
   @Input() bills: Bill[];
+
+  columnsToDisplay = ['date', 'amount'];
+  expandedElement: Bill; // TODO REDUX
 }
