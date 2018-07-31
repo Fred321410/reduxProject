8fdd28c94d08580a177a83b150a1356cea943bc1
Matthieu FLORENTIN <matthieu.florentin@netapsys.fr>
2018-07-31 18:10:21 +0200

    Essaie bouton

--- src/app/bills/components/bill-preview-list.ts
+++ src/app/bills/components/bill-preview-list.ts
@@ -1,42 +1,42 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {Bill} from '../models/bill';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { detailExpandAnimation } from '../../shared/animations';
 
 @Component({
   selector: 'rp-bill-preview-list',
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+  animations: [detailExpandAnimation],
   template: `
-    <table mat-table [dataSource]='bills'  multiTemplateDataRows class='mat-elevation-z8'>
-      <ng-container matColumnDef="{{column}}" *ngFor="let column of columnsToDisplay">
-        <th mat-header-cell *matHeaderCellDef> {{column}} </th>
-        <td mat-cell *matCellDef="let element"> {{element[column]}} </td>
-      </ng-container>
+    <div class="container-bill">
+      <button style="margin-bottom: 15px" mat-raised-button routerLink="add"><mat-icon>add</mat-icon>Ajouter</button>
+      <table mat-table [dataSource]='bills'  multiTemplateDataRows class='mat-elevation-z8'>
+        <ng-container matColumnDef="{{column}}" *ngFor="let column of columnsToDisplay">
+          <th mat-header-cell *matHeaderCellDef> {{column}} </th>
+          <td mat-cell *matCellDef="let element"> {{element[column]}} </td>
+        </ng-container>
 
-      <ng-container matColumnDef="expandedDetail">
-        <td mat-cell *matCellDef="let element" [attr.colspan]="columnsToDisplay.length">
-          <div class="example-element-detail"
-               [@detailExpand]="element == expandedElement ? 'expanded' : 'collapsed'">
-            <div class="example-element-description">
-              {{element.description}}
+        <ng-container matColumnDef="expandedDetail">
+          <td mat-cell *matCellDef="let element" [attr.colspan]="columnsToDisplay.length">
+            <div class="example-element-detail"
+                 [@detailExpandAnimation]="element == expandedElement ? 'expanded' : 'collapsed'">
+              <div class="example-element-description">
+                {{element.description}}
+              </div>
             </div>
-          </div>
-        </td>
-      </ng-container>
+          </td>
+        </ng-container>
 
-      <tr mat-header-row *matHeaderRowDef="columnsToDisplay"></tr>
-      <tr mat-row *matRowDef="let element; columns: columnsToDisplay;"
-          class="example-element-row"
-          [class.example-expanded-row]="expandedElement === element"
-          (click)="expendElement.emit(element)">
-      </tr>
-      <tr mat-row *matRowDef="let row; columns: ['expandedDetail']" class="example-detail-row"></tr>
-    </table>
+        <tr mat-header-row *matHeaderRowDef="columnsToDisplay"></tr>
+        <tr mat-row *matRowDef="let element; columns: columnsToDisplay;"
+            class="example-element-row"
+            [class.example-expanded-row]="expandedElement === element"
+            (click)="expendElement.emit(element)">
+        </tr>
+        <tr>
+
+        </tr>
+        <tr mat-row *matRowDef="let row; columns: ['expandedDetail']" class="example-detail-row"></tr>
+      </table>
+    </div>
 <!--    <rp-bill-preview *ngFor="let bill of bills" [bill]="bill"></rp-bill-preview>-->
   `,
   styleUrls: ['./bill-preview-list.scss']
