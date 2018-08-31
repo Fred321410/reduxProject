f4dd329c809385a8b584a1d07d6aa062c6935633
Matthieu FLORENTIN <matthieu.florentin@netapsys.fr>
2018-08-31 18:05:31 +0200

    Dynamic columns number grid-list

--- src/app/localisations/components/localisation-preview-list.ts
+++ src/app/localisations/components/localisation-preview-list.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { detailExpandAnimation } from '../../shared/animations';
 import { Localisation, generateMockLocalisation } from '../models/localisation';
 
@@ -8,7 +8,7 @@ import { Localisation, generateMockLocalisation } from '../models/localisation';
   template: `
     <div class="container-localisation">
       <button style="margin-bottom: 15px" mat-raised-button (click)="add()"><mat-icon>add</mat-icon>Ajouter</button>
-      <mat-grid-list cols="4" rowHeight="3:1">
+      <mat-grid-list cols="{{colsNumber}}" rowHeight="130" (window:resize)="onResize($event)">
         <mat-grid-tile *ngFor="let localisation of localisationsTest">
           <rp-localisation-preview [localisation]="localisation"></rp-localisation-preview>
         </mat-grid-tile>
@@ -20,10 +20,30 @@ import { Localisation, generateMockLocalisation } from '../models/localisation';
   `,
   styleUrls: ['./localisation-preview-list.scss']
 })
-export class LocalisationPreviewListComponent {
+export class LocalisationPreviewListComponent implements OnInit {
   @Input() localisations: Localisation[];
 
+  colsNumber: number;
+
   localisationsTest = [generateMockLocalisation()];
+
+  ngOnInit() {
+    if (window.innerWidth <= 448) {
+      this.colsNumber = 1;
+    } else {
+      this.colsNumber = (window.innerWidth % 448 > 60) ? Math.trunc(window.innerWidth / 448) : Math.trunc(window.innerWidth / 448) - 1;
+    }
+  }
+
+  onResize(event) {
+    if (event.target.innerWidth <= 448) {
+      this.colsNumber = 1;
+    } else {
+      this.colsNumber = (event.target.innerWidth % 448 > 60) ?
+        Math.trunc(event.target.innerWidth / 448) :
+        Math.trunc(event.target.innerWidth / 448) - 1;
+    }
+  }
 
   add() {
     this.localisationsTest.push(generateMockLocalisation());
