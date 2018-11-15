2bbc771c1ee0ce8f22a4b3095fe5b8ca7064eb55
Matthieu FLORENTIN <matthieu.florentin@netapsys.fr>
2018-11-15 16:47:03 +0100

    Fin création service pour type/sousType

--- _mock-server/routes/types.js
+++ _mock-server/routes/types.js
@@ -36,71 +36,36 @@ router.get('/:id', function(req, res) {
 });
 
 router.post('/', function(req, res) {
-  var db = new sqlite3.Database('./myBdd.db3')
-  var type = req.body;
-  db.run(`INSERT INTO type(name, description) VALUES(?, ?)`, [type.name, type.description], function(err) {
+  typesService.save(req.body, function (err, rows) {
     if (err) {
-      res.status(500).json(err.message);
-      return console.log(err.message);
+      console.log(err);
+      res.status(500).send(err)
+      return err;
     }
-    type.id = this.lastID;
-    type.id = type.id.toString()
-    res.status(200).json(type);
+    res.json(rows)
   });
-  db.close();
 });
 
 router.post('/:id/sousType', function(req, res) {
-  var db = new sqlite3.Database('./myBdd.db3')
-  var sousType = req.body.sousType;
-  db.serialize(() => {
-    // Queries scheduled here will be serialized.
-    db.all('SELECT name FROM type WHERE id = ' + req.params.id, function (err, row) {
-      if (err) {
-        res.status(500).json(err.message);
-        return console.log(err.message);
-      }
-      if (row.length <= 0) {
-        res.status(500).json("Ce type n'existe pas");
-        return console.log("Ce type n'existe pas");
-      }
-
-    }).run(`INSERT INTO sous_type(name, id_type)
-          VALUES(?, ?)`, [sousType, req.params.id], function (err) {
-      if (err) {
-        res.status(500).json(err.message);
-        return console.log(err.message);
-      }
-      var type = req.body.type
-      type.sousType.push(sousType)
-      res.status(200).json(type);
-    });
+  typesService.addSousType(req.params.id, req.body.sousType, function (err, rows) {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err)
+      return err;
+    }
+    res.json(rows)
   });
-  db.close();
 });
 
 router.delete('/:id/sousType/:sousType', function(req, res) {
-  var db = new sqlite3.Database('./myBdd.db3')
-  var deleteSousType = new Promise((resolve, reject) => {
-    db.all('SELECT name FROM type WHERE id = ' + req.params.id, function (err, row) {
-      if (err) {
-        res.status(500).json(err.message);
-        reject(err)
-      } else if (row.length <= 0) {
-        res.status(500).json("Ce type n'existe pas");
-        reject("Ce type n'existe pas");
-      } else {
-        resolve(row)
-      }
-    })
+  typesService.deleteSousType(req.params.id, req.params.sousType, function (err, rows) {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err)
+      return err;
+    }
+    res.json(rows)
   });
-  deleteSousType().then((row) => {
-    console.log(row);
-  }).catch((err) => {
-    console.log('Error: ');
-    console.log(JSON.stringify(err));
-  });
-  db.close();
 });
 
 
--- _mock-server/routes/types.service.js
+++ _mock-server/routes/types.service.js
@@ -12,6 +12,9 @@ service.getAll = function(cb) {
     db.all("select type.*, group_concat(st.name) as sousTypes from type\n" +
       "inner join sous_type st ON type.id = st.id_type\n" +
       "GROUP BY type.id", function(err, row){
+      if (err) {
+        cb(err.message, null)
+      }
       const results = row.map(r => {
         r.sousType = r.sousTypes.split(",");
         r.id = r.id.toString();
@@ -30,6 +33,9 @@ service.get = function(id, cb) {
       "inner join sous_type st ON type.id = st.id_type\n" +
       "WHERE type.id = " + id + "\n" +
       "GROUP BY type.id", function(err, row){
+      if (err) {
+        cb(err.message, null)
+      }
       const results = row.map(r => {
         r.sousType = r.sousTypes.split(",");
         r.id = r.id.toString();
@@ -39,5 +45,65 @@ service.get = function(id, cb) {
       cb(null, results[0])
     });
   })
+};
+
+
+service.save = function(type, cb) {
+  step(function () {
+    var db = new sqlite3.Database('./myBdd.db3');
+    db.run(`INSERT INTO type(name, description) VALUES(?, ?)`, [type.name, type.description], function(err) {
+      if (err) {
+        cb(err.message, null)
+      }
+      type.id = this.lastID;
+      type.id = type.id.toString()
+      cb(null, type)
+    });
+    db.close();
+  })
+};
+
+service.addSousType = function(idType, sousType, cb) {
+  step(function () {
+    service.get(idType, this);
+  }, function (err, results) {
+    console.log(results)
+    if (err) {
+      cb(err, null)
+    } else if (!results || results.length === 0) {
+      cb('Ce type n\'existe pas', null)
+    }
+    const type = results;
+    var db = new sqlite3.Database('./myBdd.db3');
+    db.run(`INSERT INTO sous_type(name, id_type)
+          VALUES(?, ?)`, [sousType, idType], function (err) {
+      if (err) {
+        cb(err.message, null)
+      }
+      type.sousType.push(sousType)
+      cb(null, type)
+    })
+    db.close();
+  });
+};
+
+service.deleteSousType = function(idType, sousType, cb) {
+  step(function () {
+    service.get(idType, this);
+  }, function (err, results) {
+    if (err) {
+      cb(err, null)
+    } else if (!results || results.length === 0) {
+      cb('Ce type n\'existe pas', null)
+    }
+    var db = new sqlite3.Database('./myBdd.db3');
+    db.run("DELETE FROM sous_type WHERE id_type = ? AND name = ?", [idType, sousType], function(err, row){
+      if (err) {
+        cb(err.message, null)
+      }
+      cb(null, row)
+    })
+    db.close();
+  });
 };
 
